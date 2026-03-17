9f11e76da5a34071aede73e575952ce572563e4f
aarchinakum1 <aarchiben.nakum126978@marwadiuniversity.ac.in>
2026-03-17 18:53:56 +0530

    Add files via upload

---
+++ prog18.py
@@ -0,0 +1,14 @@
+#Write a program to list and tuple.
+# Roll Number : 92400527154 : Name : Aarchi Nakum
+
+numbers = input("Enter comma-separated numbers: ")
+
+
+num_list = numbers.split(",")
+
+
+num_tuple = tuple(num_list)
+
+
+print("List:", num_list)
+print("Tuple:", num_tuple)
