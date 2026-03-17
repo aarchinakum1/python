309475d9ed402749291255f916f5374157e79d5d
aarchinakum1 <aarchiben.nakum126978@marwadiuniversity.ac.in>
2026-03-17 18:50:35 +0530

    Add files via upload

---
+++ prog9.py
@@ -0,0 +1,8 @@
+#Write a program to input a number and display table.
+#Roll number 92400527154 Name:Aarchi Nakum
+
+num = int(input("enter a number:"))
+
+for i in range(1,11):
+    print(num, "x",i,"=",num*i)
+
