50ad1c7d1769e6fcf954d4d48cc3c3e20cd37c37
aarchinakum1 <aarchiben.nakum126978@marwadiuniversity.ac.in>
2026-03-17 18:52:49 +0530

    Add files via upload

---
+++ prog15.py
@@ -0,0 +1,17 @@
+#Write a program to print factorial number.
+#Roll number 92400527154: Name:Aarchi Nakum
+
+def factorial(n):
+    fact = 1
+    for i in rang(1, n + 1):
+
+        fact = fact * i
+        return fact
+    num = int(input("enter a number: "))
+
+    print("factorial of",num,"is:",factorial(num))
+
+                   
+           
+           
+
