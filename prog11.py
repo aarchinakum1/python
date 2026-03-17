c4b8bcd5b66f50c2c921b6b9f7eddfb3db75155c
aarchinakum1 <aarchiben.nakum126978@marwadiuniversity.ac.in>
2026-03-17 18:51:01 +0530

    Add files via upload

---
+++ prog11.py
@@ -0,0 +1,10 @@
+#Write a program to calculate factorial
+# Roll Number : 92400527154 : Name : Aarchi Nakum
+
+num =int(input("enter number:"))
+fac = 1
+
+for i in range(1,num+1):
+    
+    fac=fac*i
+    print("factorial:*",fac)
